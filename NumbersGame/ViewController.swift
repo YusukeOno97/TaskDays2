d493e8652e22ffd187187d0d7c3c2a3902c3047c
小野勇輔 <onoysamutasuku@JustinBtaskMacBook.local>
2019-08-07 09:51:38 +0900

    入力履歴の作成とアラート機能の修正

--- NumbersGame/ViewController.swift
+++ NumbersGame/ViewController.swift
@@ -11,16 +11,21 @@ import UIKit
 class ViewController: UIViewController {
     
     // ランダムな数を生成
-    var random = Int.random(in: 0 ... 100)
+    var randomNum = Int.random(in: 0 ... 100)
     
-    // 現在の問題の管理
+    // ボタンを押して答えた回数
     var count: Int = 0
-    // 表示でラベル紐付け
+    
+    
+    // テキストビューの表示紐付け
     @IBOutlet weak var DisplayNum: UILabel!
     // テキスト入力紐付け
     @IBOutlet weak var TextIn: UITextField!
+    
     // 入力履歴紐付け
     @IBOutlet weak var DisplayHis: UITextView!
+    
+ 
     
     // 決定ボタン紐付け
     @IBAction func TappedButton(_ sender: Any) {
@@ -32,43 +37,77 @@ class ViewController: UIViewController {
             present(alert, animated: true, completion: nil)
         }
         
-        // 答え
-        let answer: Int = random
         
+        // 答え
+        let answer: Int = randomNum
+      
+        // 正解した後リスタートする関数
+        func restart(){
+            //カウントを初めからにする
+            count = 0
+            //  数字をシャッフル
+            randomNum = Int.random(in: 0 ... 100)
+           
+        }
        
         // Int型の数値が使われたとき
-        
         if let numA = Int(TextIn.text!) {
             count += 1
+            
+            // 入力欄を空にする
+            TextIn.text = ""
             // 入力された値が0か100以上のとき
             if numA == 0 || numA > 100{
             showlAlert(message: "「１~100の数字を入れてください」")
-                
               // 答えが入力された値より大きい場合
             } else if numA < answer {
+                
+                // アラート
                 showlAlert(message:"答えは\(numA)より大きい値です")
+                
+                //入力履歴と改行
+                DisplayHis.text += ("[\(count)] 回目 答えは\(numA)より大きい値です\n")
+
+                
+                
               // 答えが入力された値より小さい場合
             } else if numA > answer {
+                // アラート
                 showlAlert(message: "答えは\(numA)より小さい値です")
+                
+                //入力履歴と改行
+                DisplayHis.text += ("[\(count)] 回目 答えは\(numA)より小さい値です\n")
+              
+                
               //答えが入力された値と一致する場合
             } else if numA == answer{
-                showlAlert(message: "正解です")
+                // アラート
+                showlAlert(message: "\(count)回目で正解しました。\n数字をリセットしました。")
+                //入力履歴と改行
+                DisplayHis.text += ("[正解] 答えは\(answer)でした。\n")
+                // 数値をリスタートする
+                restart()
             }
+            
+            
+         
+            
+
             
             return
         }
         
-        //Int型以外の型を入れたとき（絵文字対応）
+        //Int型以外の型を入れたとき
         if String?(TextIn.text!) != nil{
+           // アラートの表示
             showlAlert(message: "「１~100の数字を入れてください」")
+           
+            // 入力欄を空にする
+            TextIn.text = ""
+            
         }
         
         
-        
-        
-        
-        
-    
         
     }
    
@@ -77,6 +116,7 @@ class ViewController: UIViewController {
         
         
         super.viewDidLoad()
+          DisplayHis.isEditable = false
         
     }
     
